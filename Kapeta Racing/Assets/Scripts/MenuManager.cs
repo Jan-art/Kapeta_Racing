d162fd64bdacb854e16fa1f4a4ab90ac2dc1319b
Jan-art <JanSingh.Official@gmail.com>
2023-02-28 20:25:19 +0000

    Added Menu + Key Inputs

---
+++ Kapeta Racing/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Game exiting");
+            Application.Quit();
+
+        }
+    }
+
+    
+   public void Load(string sceneToLoad)
+    {
+        SceneManager.LoadScene("Circuit Track");
+        Debug.Log("Game loaded");
+
+    }
+
+    public void ExitGame()
+    {
+     Application.Quit();
+     Debug.Log("Game exiting");
+    }
+
+
+}
